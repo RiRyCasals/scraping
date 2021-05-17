d9fbc8ae56d7b49d65f59a7be6c012ebe112acb2
RiRyCasals <RiRy@example.com>
2021-05-17 21:41:44 +0900

    changed the variable name

--- images_scraping.py
+++ images_scraping.py
@@ -7,42 +7,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def download_imgs(img_urls, save_dir):
+def download_imgs(img_urls, save_directory):
     for i, url in enumerate(img_urls):
         file_name = '{}.png'.format(i)
-        save_img_path = os.path.join(save_dir, file_name)
+        save_img_path = os.path.join(save_directory, file_name)
         r = requests.get(url, stream=True)
         if r.status_code == 200:
             with open(save_img_path, 'wb') as f:
                 f.write(r.content)
-        if (i + 1) % 10 == 0:
+        if (i + 1) % 50 == 0:
             print('{} / {} done'.format((i+1), len(img_urls)))
         if (i + 1) == len(img_urls):
             print('complete')
 
 
 word = 'チェロ'
-save_dir = './images/cello'
+save_directory = './images/cello'
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
 
 options = Options()
 # options.add_argument('--headless')   # 挙動確認のためコメントアウト
 driver = webdriver.Chrome('./chromedriver.exe',options=options)
 
-url = 'https://search.yahoo.co.jp/image/search?p={}'
-driver.get(url.format(word))
+driver.get('https://search.yahoo.co.jp/image/search?p={}'.format(word))
 
-urls = deque()
+image_urls = deque()
 
 while True:
-    prev_html = driver.page_source
+    preview_html = driver.page_source
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
     sleep(1.0)
     current_html = driver.page_source
-    if prev_html != current_html:
-        prev_html = current_html
+    if preview_html != current_html:
+        preview_html = current_html
     else:
         try:
             button = driver.find_element_by_class_name('sw-Button')
@@ -54,8 +53,8 @@ while True:
 elements = driver.find_elements_by_tag_name('img')
 for elem in elements:
     url = elem.get_attribute('src')
-    if url not in urls:
-        urls.append(url)
+    if url not in image_urls:
+        image_urls.append(url)
 
 driver.close()
-download_imgs(urls, save_dir)
+download_imgs(image_urls, save_directory)
