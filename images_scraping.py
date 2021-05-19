dc4ad0a6f9d14a4e90eca66d32819c3beba0f289
RiRyCasals <RiRy@example.com>
2021-05-19 22:26:40 +0900

    rganized code and variable names

--- images_scraping.py
+++ images_scraping.py
@@ -7,54 +7,78 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def download_imgs(img_urls, save_directory):
+def make_save_directory(save_directory_path):
+    if not os.path.exists(save_directory_path):
+        os.makedirs(save_directory_path)
+
+
+def obtain_html(driver):
+    while True:
+        preview_html = driver.page_source
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        sleep(1.0)
+        current_html = driver.page_source
+
+        if preview_html != current_html:
+            preview_html = current_html
+        else:
+            try:
+                button = driver.find_element_by_class_name('sw-Button')
+                button.click()
+            except:
+                break
+
+    return driver
+
+
+def obtain_image_urls(driver):
+    image_urls = deque()
+    elements = driver.find_elements_by_tag_name('img')
+    for elem in elements:
+        url = elem.get_attribute('src')
+
+        if url not in image_urls:
+            image_urls.append(url)
+
+    return image_urls
+
+
+def save_image(request, save_image_path):
+    with open(save_image_path, 'wb') as f:
+        f.write(request.content)
+
+
+def download_images_from_urls(img_urls, save_directory_path):
+    print("start download")
     for i, url in enumerate(img_urls):
-        file_name = '{}.png'.format(i)
-        save_img_path = os.path.join(save_directory, file_name)
-        r = requests.get(url, stream=True)
-        if r.status_code == 200:
-            with open(save_img_path, 'wb') as f:
-                f.write(r.content)
-        if (i + 1) % 50 == 0:
+        request = requests.get(url, stream=True)
+        if request.status_code != 200:
+            continue
+
+        save_image_path = os.path.join(save_directory_path,
+                                        '{:3}.png'.format(i))
+        save_image(request, save_image_path)
+
+        if (i + 1) % 10 == 0: # 最終的に削除
             print('{} / {} done'.format((i+1), len(img_urls)))
-        if (i + 1) == len(img_urls):
-            print('complete')
+
+    print('complete')
 
 
-word = 'チェロ'
-save_directory = './images/cello'
-
-if not os.path.exists(save_directory):
-    os.makedirs(save_directory)
 
 options = Options()
-# options.add_argument('--headless')   # 挙動確認のためコメントアウト
+# options.add_argument('--headless') # 最終的に解除
 driver = webdriver.Chrome('./chromedriver.exe',options=options)
 
-driver.get('https://search.yahoo.co.jp/image/search?p={}'.format(word))
+scraping_word = 'モルカー'
+driver.get('https://search.yahoo.co.jp/image/search?p={}'.format(scraping_word))
+driver = obtain_html(driver)
 
-image_urls = deque()
+save_directory_path = os.path.join('./images', scraping_word)
+make_save_directory(save_directory_path)
 
-while True:
-    preview_html = driver.page_source
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-    sleep(1.0)
-    current_html = driver.page_source
-    if preview_html != current_html:
-        preview_html = current_html
-    else:
-        try:
-            button = driver.find_element_by_class_name('sw-Button')
-            button.click()
-        except:
-            break
-
-
-elements = driver.find_elements_by_tag_name('img')
-for elem in elements:
-    url = elem.get_attribute('src')
-    if url not in image_urls:
-        image_urls.append(url)
+image_urls = obtain_image_urls(driver)
 
 driver.close()
-download_imgs(image_urls, save_directory)
+
+download_images_from_urls(image_urls, save_directory_path)
